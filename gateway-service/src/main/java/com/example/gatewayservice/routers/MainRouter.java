452ae6f34a8655d5d8037cb3b5cbbaae0358adb5
Camilo del Real <cjdelreal2009@gmail.com>
2021-11-03 00:01:48 +0100

    ADD: Gateway service

--- gateway-service/src/main/java/com/example/gatewayservice/routers/MainRouter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/routers/MainRouter.java
@@ -1,2 +1,54 @@
-package com.example.gatewayservice.routers;public class MainRouter {
+package com.example.gatewayservice.routers;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MainRouter {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(r -> r
+                        .path("/users/login")
+                        .and()
+                        .method(HttpMethod.POST)
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                        )
+                        .uri("lb://userservice")
+                )
+                .route(r -> r
+                        .path("/users/**")
+                        .and()
+                        .method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                        )
+                        .uri("lb://userservice")
+                )
+                .route(r -> r
+                        .path("/categories/**")
+                        .and()
+                        .method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                        )
+                        .uri("lb://productservice")
+                )
+                .route(r -> r
+                        .path("/products/**")
+                        .and()
+                        .method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                        )
+                        .uri("lb://productservice")
+                )
+                .build();
+    }
+
 }
